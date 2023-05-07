19f8f2c300d914e380ae86a5d21f2f5948e0caf3
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:50:20 +0530

    Update ProductController.java

--- src/main/java/com/geekster/Ecommerce/controller/ProductController.java
+++ src/main/java/com/geekster/Ecommerce/controller/ProductController.java
@@ -1,4 +1,32 @@
 package com.geekster.Ecommerce.controller;
 
+import com.geekster.Ecommerce.model.ProductModel;
+import com.geekster.Ecommerce.service.ProductService;
+import jakarta.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/")
+    public String addProduct(@RequestBody List<ProductModel> products) {
+        return productService.addProduct(products);
+    }
+
+    @GetMapping("/products")
+    public List<ProductModel> getProduct(@Nullable @RequestParam String category) {
+        return productService.getProduct(category);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public String removeProduct(@PathVariable Integer productId) {
+        return productService.removeProduct(productId);
+    }
+
 }
