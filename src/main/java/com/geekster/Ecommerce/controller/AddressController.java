f7cd9790cfecf6e10d7658a947b9d896783a30fa
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:49:23 +0530

    Update AddressController.java

--- src/main/java/com/geekster/Ecommerce/controller/AddressController.java
+++ src/main/java/com/geekster/Ecommerce/controller/AddressController.java
@@ -1,4 +1,20 @@
 package com.geekster.Ecommerce.controller;
 
+import com.geekster.Ecommerce.model.AddressModel;
+import com.geekster.Ecommerce.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AddressController {
+
+    @Autowired
+    AddressService addressService;
+
+    @PostMapping("/")
+    public String addAddress(@RequestBody AddressModel address){
+        return addressService.addAddress(address);
+    }
 }
