3856fb69609aaf3ea86bc14cc23d45d6d7fcc162
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:49:52 +0530

    Update OrderController.java

--- src/main/java/com/geekster/Ecommerce/controller/OrderController.java
+++ src/main/java/com/geekster/Ecommerce/controller/OrderController.java
@@ -1,4 +1,22 @@
 package com.geekster.Ecommerce.controller;
 
+import com.geekster.Ecommerce.model.OrderModel;
+import com.geekster.Ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("/")
+    public String addOrders(@RequestBody OrderModel order){
+        return orderService.addOrders(order);
+    }
+    @GetMapping("/order")
+    public OrderModel getOrders(@RequestParam Integer orderId){
+        return orderService.getOrders(orderId);
+    }
 }
