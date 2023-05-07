f5250434e43a5bd0996e4520a03229774b384504
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:52:48 +0530

    Update OrderModel.java

--- src/main/java/com/geekster/Ecommerce/model/OrderModel.java
+++ src/main/java/com/geekster/Ecommerce/model/OrderModel.java
@@ -13,10 +13,21 @@ import lombok.NoArgsConstructor;
 public class OrderModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserModel userId;
 
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductModel productId;
 
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private AddressModel addressId;
+
+    @Column(name = "product_quantity")
     private Integer productQuantity;
 }
