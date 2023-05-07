dbd03940f7bc2b54e68b9025e84158f73080ba3d
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:55:56 +0530

    Update IAddressDao.java

--- src/main/java/com/geekster/Ecommerce/repository/IAddressDao.java
+++ src/main/java/com/geekster/Ecommerce/repository/IAddressDao.java
@@ -1,4 +1,9 @@
 package com.geekster.Ecommerce.repository;
 
-public interface IAddressDao {
+import com.geekster.Ecommerce.model.AddressModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IAddressDao extends JpaRepository<AddressModel,Integer> {
 }
