b098e5cd954710e0599d180d1d97a0bc09be06b4
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:57:32 +0530

    Update IUserDao.java

--- src/main/java/com/geekster/Ecommerce/repository/IUserDao.java
+++ src/main/java/com/geekster/Ecommerce/repository/IUserDao.java
@@ -1,4 +1,9 @@
 package com.geekster.Ecommerce.repository;
 
-public interface IUserDao {
+import com.geekster.Ecommerce.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserDao extends JpaRepository<UserModel,Integer> {
 }
