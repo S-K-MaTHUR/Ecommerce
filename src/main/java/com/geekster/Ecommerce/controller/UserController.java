60ccd693451d6d2e59a2c054404c5728e384112d
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:50:50 +0530

    Update UserController.java

--- src/main/java/com/geekster/Ecommerce/controller/UserController.java
+++ src/main/java/com/geekster/Ecommerce/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.geekster.Ecommerce.controller;
 
+import com.geekster.Ecommerce.model.UserModel;
+import com.geekster.Ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping()
+    public String addUsers(@RequestBody List<UserModel> users){
+        return userService.addUsers(users);
+    }
+    @GetMapping()
+    public UserModel getUser(@RequestParam Integer userId) {
+        return userService.getUser(userId);
+    }
 }
