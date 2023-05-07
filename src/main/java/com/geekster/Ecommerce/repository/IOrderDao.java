66e82cef4e2fd823d48abe1eabda78e6fbe08270
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-07 23:56:35 +0530

    Update IOrderDao.java

--- src/main/java/com/geekster/Ecommerce/repository/IOrderDao.java
+++ src/main/java/com/geekster/Ecommerce/repository/IOrderDao.java
@@ -1,4 +1,9 @@
 package com.geekster.Ecommerce.repository;
 
-public interface IOrderDao {
+import com.geekster.Ecommerce.model.OrderModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IOrderDao extends JpaRepository<OrderModel,Integer> {
 }
